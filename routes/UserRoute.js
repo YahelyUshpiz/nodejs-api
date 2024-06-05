3ca4a8f4194f7658fa4087ac866b9a73fa0d413a
YahelyUshpiz <yahely31@gmail.com>
2024-06-05 20:28:50 +0300

    fix

--- routes/UserRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as UserController from "../controllers/UserController.js";
-
-export const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json(UserController.getAllUsers(req, res));
-});
